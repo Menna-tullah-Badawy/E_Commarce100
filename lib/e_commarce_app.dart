4639a833acb952085184f439c3294e29ac5266ea
mennatullahbadawy <mennatullahbadawy@gmail.com>
2024-12-19 09:02:50 +0000

    commit1

--- lib/e_commarce_app.dart
+++ lib/e_commarce_app.dart
@@ -1,17 +1,15 @@
+
 import 'package:flutter/material.dart';
 
-class E_Commarce100 extends StatelessWidget {
-  const E_Commarce100({super.key});
+class ECommarce100 extends StatelessWidget {
+  const ECommarce100({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'E-commarce100',
       
-      
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      home: Scaffold(appBar: AppBar(title: Text("E-commarce100")),),
     );
   }
 }
